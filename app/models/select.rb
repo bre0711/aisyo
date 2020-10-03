706aa96cb2987e1800c238b1b2ddcfc39bf2a0c7
bre0711 <brezelgukptp0@gmail.com>
2020-10-03 10:48:51 +0900

    READ ME編集

--- app/models/select.rb
+++ app/models/select.rb
@@ -1,10 +1,10 @@
 class Select < ActiveHash::Base
   self.data = [
   { id: 1, name: '--' },
-  { id: 2, name: '剣士' },
-  { id: 3, name: '魔法使い' },
-  { id: 4, name: '僧侶' },
-  { id: 5, name: '格闘家' },
-  { id: 6, name: '銃使い' }
+  { id: 2, name: 'Soldier' },
+  { id: 3, name: 'Wizard' },
+  { id: 4, name: 'Monk' },
+  { id: 5, name: 'Fighter' },
+  { id: 6, name: 'Gunner' }
 ]
 end
